c3829e62da6d1e069f576d8ab8d34c2252baed38
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-26 11:55:52 -0300

    Adding a method to validate the loging

--- dustwitter/dust_auth/views.py
+++ dustwitter/dust_auth/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
-def login(request):
+def show_login(request):
     if request.method == "GET":
         return render(request, "dust_auth/login.html")
     else:
-        make_login(request)
-        return render(request, "dust_auth/login.html")
+        context = make_login(request)
+
+        if context.get('is_logged'):
+            return redirect(reverse("home:index"))
+        else:
+            return render(request, "dust_auth/login.html", context)
 
 def make_login(request):
     form = request.POST
-    print(form.get('username'))
-    print(form.get('password'))
+    username = form.get('username')
+    password = form.get('password')
+
+    user = authenticate(username=username, password=password)
+    is_logged = False
+
+    if user is not None:
+        login(request, user)
+        message = "Logged"
+        is_logged = True
+    else:
+        message = "Incorrect user"
+
+    context = {
+        "is_logged": is_logged,
+        "message": message,
+    }
+
+    return context
