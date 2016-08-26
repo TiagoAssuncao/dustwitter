09c7173fa1720fdf401d250a5ff3dd0a5ba25009
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-26 10:55:17 -0300

    Adding method new

--- dustwitter/comment/views.py
+++ dustwitter/comment/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from comment.models import Comment
 
 # Create your views here.
+def new(request):
+    form = request.POST
+    comment_text = form.get('comment')
+
+    comment = Comment.objects.create()
+    comment.comment = comment_text
+    comment.save()
+
+    return redirect(reverse('home:index'))
