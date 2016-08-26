3be631a86ec9416ee4881fb701d28406cfa418d3
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-26 13:31:29 -0300

    Adding a model field to author on comment
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- dustwitter/comment/models.py
+++ dustwitter/comment/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Comment(models.Model):
     comment = models.TextField()
+    author = models.ForeignKey(User)
