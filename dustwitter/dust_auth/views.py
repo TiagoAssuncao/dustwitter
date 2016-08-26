e5bb29875887ecceec3a24f9be9550069057268a
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-26 13:08:43 -0300

    Adding models to auth
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- dustwitter/dust_auth/views.py
+++ dustwitter/dust_auth/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def show_login(request):
