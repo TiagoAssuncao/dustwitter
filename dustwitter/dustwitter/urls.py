7b63a5d8afec6888228af2330110cf28db36c747
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-08-26 10:55:55 -0300

    Adding url to app comment

--- dustwitter/dustwitter/urls.py
+++ dustwitter/dustwitter/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^comment/', include('comment.urls', namespace='comment')),
     url(r'^', include('home.urls', namespace='home')),
 ]
